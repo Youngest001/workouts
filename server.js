7ef779d230b59a39b72a349d1b70baeddddb734b
Youngest001 <ifechukwusolomon090@gmail.com>
2024-10-17 16:02:55 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require ('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const cors = require('cors')
+
+// express app
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+//connect to database
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    // listen for request
+    app.listen(process.env.PORT, () =>{
+        console.log('connected to db & listening on port 4000')
+    })
+})
+.catch((error) =>{
+    console.log(error)
+})
