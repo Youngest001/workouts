eb686fa6646d729d01b0fb9ce98b59eb3757ba02
Youngest001 <ifechukwusolomon090@gmail.com>
2024-10-17 16:11:10 +0100

    bug fix

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     // listen for request
     app.listen(process.env.PORT, () =>{
-        console.log('connected to db & listening on port 4000')
+        console.log(`connected to db & listening on port ${process.env.PORT}`)
     })
 })
 .catch((error) =>{
